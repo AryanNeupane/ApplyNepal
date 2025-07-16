0531094b061ca4d0190f1b5d58a0a041407b3aee
Aryan Neupane <official.aryanneupane@gmail.com>
2025-07-16 07:58:57 +0545

    handling login form

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form className="bg-white text-gray-500 max-w-[350px] mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
-        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">Login Now</h2>
-        
+    <div className="flex justify-center items-center min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white text-gray-500 max-w-[350px] mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10"
+      >
+        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
+          Login Now
+        </h2>
+
         <input
-          id="email"
+          name="email"
           className="w-full border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
           type="email"
+          value={formData.email}
+          onChange={handleChange}
           placeholder="Enter your email"
           required
         />
 
         <input
-          id="password"
+          name="password"
           className="w-full border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
           type="password"
+          value={formData.password}
+          onChange={handleChange}
           placeholder="Enter your password"
           required
         />
 
-        <div className="text-right py-4">
-          <a className="text-blue-600 underline" href="#">Forgot Password?</a>
-        </div>
-
         <button
           type="submit"
-          className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600/90 active:scale-95 transition py-2.5 rounded-full text-white"
+          className="w-full mb-3 my-5 bg-primary hover:bg-indigo-600/90 active:scale-95 transition py-2.5 rounded-full text-white"
         >
           Log in
         </button>
 
         <p className="text-center mt-4">
-          Don’t have an account?{' '}
-          <a href="#" className="text-blue-500 underline">Signup Now</a>
+          Don’t have an account?{" "}
+          <Link to="/signup" className="text-blue-500 underline">
+            Signup Now
+          </Link>
         </p>
       </form>
     </div>
