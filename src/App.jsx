082416d877e22566ec41b8b77c30d15c293e8dfb
Aryan Neupane <official.aryanneupane@gmail.com>
2025-07-17 10:07:49 +0545

    setting up hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import Home from './pages/Home';
-import AllJobs from './pages/AllJobs';
-import JobDetails from './pages/JobDetails';
-import About from './pages/About';
-import Signup from './pages/auth/Signup';
-import Login from './pages/auth/Login';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Home from "./pages/Home";
+import AllJobs from "./pages/AllJobs";
+import JobDetails from "./pages/JobDetails";
+import About from "./pages/About";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const adminPath=useLocation().pathname.includes("admin")
-  const employerPath=useLocation().pathname.includes("employer")
+  const adminPath = useLocation().pathname.includes("admin");
+  const employerPath = useLocation().pathname.includes("employer");
   return (
     <>
       <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        {adminPath|| employerPath? null :<Navbar />}
-      </div>
+        {adminPath || employerPath ? null : <Navbar />}
 
-      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/all-jobs' element={<AllJobs />} />
-          <Route path='/job-details/:id' element={<JobDetails />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/all-jobs" element={<AllJobs />} />
+          <Route path="/job-details/:id" element={<JobDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-      </div>
-        {adminPath|| employerPath? null :<Footer />}
 
+        {adminPath || employerPath ? null : <Footer />}
+      </div>
     </>
   );
 };
